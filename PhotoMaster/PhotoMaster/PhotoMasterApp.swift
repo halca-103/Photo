4c783dae7b516fe4e6ea619f720d14aa95711bac
haruka <yaoxiang.elec103@gmail.com>
2026-02-10 14:57:39 +0900

    Add files via upload

---
+++ PhotoMaster/PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by Fukushima Haruka on 2026/02/09.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
